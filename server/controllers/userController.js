7af4491c66c721144444397c3aed59fe96b547f4
Ayush Kumar <akofficial1330@gmail.com>
2025-09-07 20:51:06 +0530

    Refactor

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,14 @@
 export const getProfile = async (req, res) => {
   try {
-    res.json({ success: true, message: "Protected data", user: req.user });
+    return res.status(200).json({
+      success: true,
+      message: "Protected data",
+      user: req.user,
+    });
   } catch (err) {
-    console.error("Protected route error:", err);
-    res.status(500).json({ success: false, message: "Server error" });
+    console.error("Get profile error:", err);
+    return res
+      .status(500)
+      .json({ success: false, message: "Server error during profile fetch" });
   }
 };
