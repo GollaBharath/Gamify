4f7ea16f8b888aeb57e3a9e4eeb2aadc17875748
KGFCH2 <babinbid05@gmail.com>
2026-06-02 14:04:52 +0530

    fix: align OAuth redirect env var with FRONTEND_URL

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,7 @@ import jwt from "jsonwebtoken";
 
 import { register, login } from "../controllers/authController.js";
 
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+const CLIENT_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 const router = express.Router();
 
