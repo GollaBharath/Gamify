76d1df5d017a4178520dbd3f342d9a2da2740bd4
KGFCH2 <babinbid05@gmail.com>
2026-06-02 13:54:27 +0530

    fix: wrap app with ThemeProvider for theme hook stability

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,23 +3,26 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import App from "./App.jsx";
+import { ThemeProvider } from "./Context/ThemeContext.jsx";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-			<ToastContainer
-				position="bottom-right"
-				autoClose={3500}
-				hideProgressBar={false}
-				newestOnTop
-				closeOnClick
-				pauseOnHover
-				theme="dark"
-			/>
-		</BrowserRouter>
+		<ThemeProvider>
+			<BrowserRouter>
+				<App />
+				<ToastContainer
+					position="bottom-right"
+					autoClose={3500}
+					hideProgressBar={false}
+					newestOnTop
+					closeOnClick
+					pauseOnHover
+					theme="dark"
+				/>
+			</BrowserRouter>
+		</ThemeProvider>
 	</React.StrictMode>,
 );
 
