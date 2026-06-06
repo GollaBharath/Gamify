d944558e85d840527a1518c136a3d4f84c88a82d
KGFCH2 <babinbid05@gmail.com>
2026-06-06 18:43:49 +0530

    refactor: add input validation middleware for register and login routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,6 +6,7 @@ import passport from "passport";
 import jwt from "jsonwebtoken";
 
 import { register, login } from "../controllers/authController.js";
+import { validateRegistration, validateLogin } from "../middlewares/validationMiddleware.js";
 
 const CLIENT_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
@@ -48,8 +49,8 @@ const registerLimiter = rateLimit({
 });
 
 // Apply rate limiters to specific routes
-router.post("/register", registerLimiter, register);
-router.post("/login", loginLimiter, login);
+router.post("/register", registerLimiter, validateRegistration, register);
+router.post("/login", loginLimiter, validateLogin, login);
 router.post('/refresh', async (req, res) => {
   // Add refresh token logic here
 });
